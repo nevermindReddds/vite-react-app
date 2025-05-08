99b3ca96facc0ab5c5339744dde540c59f6a2f84
nevermindReddds <vsetop1981@gmail.com>
2025-05-08 21:10:02 +0200

    text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import Header from "./components/Header";
 
+function WayToTeach() {
+  return (
+    <ul>
+      <li>
+        <p>
+          <strong>Информация и технологии</strong>
+        </p>
+      </li>
+    </ul>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h1>hello world</h1>
+        <section>
+          <h3>наш подход к обучению</h3>
+
+          <ul>
+            <li>
+              <p>
+                <strong>формат обучения</strong>
+              </p>
+            </li>
+          </ul>
+        </section>
       </main>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
-const now = new Date();
+import logo from "/mid_348779_607834.jpg";
 
 export default function Header() {
+  const now = new Date();
+
   return (
     <header>
+      <img src={logo} alt={"result"} />
       <h3>Result University</h3>
-      <span>Время сейчас: {now.toLocaleTimeString} </span>
+      <span>Время сейчас: {now.toLocaleTimeString()} </span>
     </header>
   );
 }
